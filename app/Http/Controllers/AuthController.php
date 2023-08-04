a3d93a3209e6e03fc24ae9cb5905b4ba18597397
khinezinthantthant <khinezinthant.dev@gmail.com>
2023-08-04 13:23:23 +0630

    authentication completed

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+
+    public function login(Request $request)
+    {
+        // return Auth::user()->password;
+        $request->validate([
+            "email" => "required|email",
+            "password" => "required|min:8"
+        ]);
+
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response()->json([
+                "message" => "Username or password wrong",
+            ]);
+        }
+
+        return Auth::user()->createToken($request->has("device") ? $request->device : 'unknown');
+    }
+
+    public function logout()
+    {
+        Auth::user()->currentAccessToken()->delete();
+        return response()->json([
+            "message" => "logout successful"
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => "khine zin",
+            'email' => 'kzt@gmail.com',
+            'password' => Hash::make("asdffdsa"),
+            "role" => "admin"
+        ]);
+        User::factory()->create([
+            'name' => "moe moe aung",
+            'email' => 'mma@gmail.com',
+            'password' => Hash::make("asdffdsa"),
+            "role" => "staff"
+        ]);
+    }
+}
